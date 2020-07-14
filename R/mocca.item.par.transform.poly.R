3cac628a7d46e86a9020aa0ed9f2e9fc41b6c0db
Yikai Lu <luyikei.qmltu@gmail.com>
2020-07-15 03:20:45 +0900

    Revert "grm: Use a*b_k instead of b_k"
    This reverts commit 5e94812b100e5c95e3fd11bd142662cd71badcb2.

--- R/mocca.item.par.transform.poly.R
+++ R/mocca.item.par.transform.poly.R
@@ -3,8 +3,8 @@ mocca.item.par.transform.poly <- function(item_par){
     if(item_par[i,1]==0){
       item_par[i,] <- item_par[i,]
     } else {
-      item_par[i,2] <- -1*item_par[i,2]
-      item_par[i,3] <- -1*item_par[i,3]
+      item_par[i,2] <- -1*(item_par[i,2]/item_par[i,1])
+      item_par[i,3] <- -1*(item_par[i,3]/item_par[i,1])
     }
   }
   return(item_par)
